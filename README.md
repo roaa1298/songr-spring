4f40e9366e4e233da8c50949f1c457c62236e20b
roaa1998 <roaabhailat4@gmail.com>
2022-03-21 22:05:59 +0300

    add albums

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
    - for hello world page : (http://localhost:8080/hello) .
    - for capitalizer page : (http://localhost:8080/capitalize/word).
    - for albums page : (http://localhost:8080/albums)
+   - to add album in the albums page : (http://localhost:8080/addAlbum)
+
+- before we run our code we should start the postgres server by **sqlstart** command. and then we can connect to our database or create a database if we didn't create it before by **psql** command, and then we can run our application and see our table that is generated from the application.
--- src/main/java/com/example/songr/controllers/AlbumController.java
+++ src/main/java/com/example/songr/controllers/AlbumController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,20 +21,41 @@ public class AlbumController {
     AlbumRepository albumRepository;
 
 
-    @ResponseBody
-    @GetMapping("/albums")
-    public List<Album> getAlbums(Model model){
-        Album album1 = new Album("For life", "EXO",10,  350, "https://en.wikipedia.org/wiki/For_Life_(EP)#/media/File:For_Life_EP_Poster.jpg");
-        Album album2 = new Album("Blue", "Joni Mitchell", 4,  407, "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440");
-        Album album3 = new Album("Dynamite","BTS", 8,  350,"https://en.wikipedia.org/wiki/Dynamite_(BTS_song)#/media/File:BTS_-_Dynamite_(official_cover).png");
-        List<Album> albums = new ArrayList<>() ;
-        albums.add(album1);
-        albums.add(album2);
-        albums.add(album3);
-        albumRepository.save(album1);
-        albumRepository.save(album2);
-        albumRepository.save(album3);
-        model.addAttribute("albums",albums);
-        return albums;
+//    @ResponseBody
+//    @GetMapping("/albums")
+//    public String getAlbums(Model model){
+//        Album album1 = new Album("For life", "EXO",10,  350, "https://en.wikipedia.org/wiki/For_Life_(EP)#/media/File:For_Life_EP_Poster.jpg");
+//        Album album2 = new Album("Blue", "Joni Mitchell", 4,  407, "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1561488440");
+//        Album album3 = new Album("Dynamite","BTS", 8,  350,"https://en.wikipedia.org/wiki/Dynamite_(BTS_song)#/media/File:BTS_-_Dynamite_(official_cover).png");
+//        List<Album> albums = new ArrayList<>() ;
+//        albums.add(album1);
+//        albums.add(album2);
+//        albums.add(album3);
+//        albumRepository.save(album1);
+//        albumRepository.save(album2);
+//        albumRepository.save(album3);
+//        model.addAttribute("albums",albumRepository.findAll());
+//        return "album";
+//    }
+
+    @GetMapping("/addAlbum")
+    public String addAlbumForm(){
+        return "addAlbum";
     }
+
+    @PostMapping("/addAlbum")
+    public RedirectView addAlbum(@ModelAttribute Album album) {
+        System.out.println(album);
+        albumRepository.save(album);
+        return new RedirectView("albums");
+    }
+
+    @GetMapping("/albums")
+    public String getAllAlbums(Model model){
+        model.addAttribute("albumsList",albumRepository.findAll());
+        return "album";
+    }
+
+
+
 }
